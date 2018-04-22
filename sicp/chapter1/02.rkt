bf586d6fc75f8280b854363da829a5a2b68d1ad1
i-obr <obr.iv@ya.ru>
2018-04-22 23:33:03 +0300

    init

---
+++ sicp/chapter1/02.rkt
@@ -0,0 +1,17 @@
+#lang racket/base
+
+(require rackunit)
+
+(define solution (
+  /
+  (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
+  (* 3 (- 6 2) (- 2 7))
+))
+
+(module+ test
+
+  (test-case
+    "1_02"
+    (check-equal? solution -37/150)
+  )
+)
